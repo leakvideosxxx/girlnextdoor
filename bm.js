f5577d9230bae4eb9542207dcac45335a6c192a5
leakvideosxxx <91283145+leakvideosxxx@users.noreply.github.com>
2021-10-16 15:36:30 +0100

    Add files via upload

---
+++ bm.js
@@ -0,0 +1,15 @@
+
+// setTimeout(window.open("https://www.youraddress.com"),4);
+let x=0;
+// if(x<5){
+//     setTimeout(()=>{
+//         window.open("https://www.youraddress.com");
+//     },600);
+//     x++;
+// }
+
+    setInterval(()=>{
+                window.open("//ashoupsu.com/4/4594571");
+           },7000);
+       
+
